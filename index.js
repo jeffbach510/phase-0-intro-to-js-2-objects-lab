b2035b33e3fe84c69bc854b0664945a515bb06d0
Jeffrey Bachman <jeffbach510@sbcglobal.net>
2023-03-09 16:05:23 -0800

    changes made to index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = "Jeff";
+employee.address ="20 Park Place";
+employee;
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+const newEmp = { ...employee};
+newEmp[key] = value;
+return newEmp;
+};
+
+const newEmp = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Bob"
+  );
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)  {
+    employee[key] = value;
+    return employee;
+ };
+
+ destructivelyUpdateEmployeeWithKeyAndValue(employee, employee.name, "Sam");
+
+
+ function deleteFromEmployeeByKey(employee, name)  {
+ const newEmployee = { ...employee};
+ delete newEmployee.name;
+ return newEmployee;
+ };
+
+
+ deleteFromEmployeeByKey(employee, "Sam");
+
+ function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+ };
+
+ destructivelyDeleteFromEmployeeByKey(employee, key);
